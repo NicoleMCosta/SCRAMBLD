054514bad0a6d90418adfe7e8edec77f5e9aa2e1
NicoleMCosta <nicolemartinsdacosta@gmail.com>
2025-11-06 16:53:41 -0300

    mudanças no css

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,35 @@ function App() {
     <>
       <form className="flex-col items-center w-screen h-screen">
 
-        <div id="categories" className="flex justify-between items-center m-10">     
-          <div className="flex items-center gap-2">
+        <div id="categories" className="flex flex-nowrap justify-between items-center m-10">    
+           {/*TODO responsividade das categorias em telas menores  */}
+
             <div className="cat_image" style={{backgroundImage: "url('./src/assets/hkty.jpeg')"}}>categoria 1 </div> 
-          </div>
-          <div className="h-18 bg-linear-to-r from-green-300 to-pink-500 rounded-2xl px-4 py-2 shadow-md content-center">categoria 2</div>
-          <div className="h-18 bg-linear-to-r from-green-300 to-yellow-300 rounded-2xl px-4 py-2 shadow-md content-center">categoria 3</div> 
-            {/*imagem no fundo das categorias ou apenas cor e texto?*/}
+            <div className="cat_image" style={{backgroundImage: "url('./src/assets/hkty.jpeg')"}}>categoria 1 </div> 
+            <div className="cat_image" style={{backgroundImage: "url('./src/assets/hkty.jpeg')"}}>categoria 1 </div>             
+            <div className="cat_image" style={{backgroundImage: "url('./src/assets/hkty.jpeg')"}}>categoria 1 </div> 
+            <div className="cat_image" style={{backgroundImage: "url('./src/assets/hkty.jpeg')"}}>categoria 1 </div> 
+            <div className="cat_image" style={{backgroundImage: "url('./src/assets/hkty.jpeg')"}}>categoria 1 </div>             
+            <div className="cat_image" style={{backgroundImage: "url('./src/assets/hkty.jpeg')"}}>categoria 1 </div> 
+            <div className="cat_image" style={{backgroundImage: "url('./src/assets/hkty.jpeg')"}}>categoria 1 </div> 
+            <div className="cat_image" style={{backgroundImage: "url('./src/assets/hkty.jpeg')"}}>categoria 1 </div>             
+            <div className="cat_image" style={{backgroundImage: "url('./src/assets/hkty.jpeg')"}}>categoria 1 </div> 
         </div>
         
-        <div className='flex-col justify-center items-center w-screen'>
-
-          <div className='text-8xl px-4 mb-0 md:px-10 md:text-8xl'>SCRAMBLE</div> {/* usar imagem para a logo? */}
-
+        <div className='flex flex-col justify-center items-center w-screen'>
+          
           <div className="mb-5r flex flex-col items-center border-2 border-amber-300">
-            <input type="text" id="words" className="md:basis-2/5 max-w h-20 p-5 bg-amber-200 text-pink-500 text-m rounded-full hover:border-4 hover:border-amber-600   ]" 
-            placeholder="Digite suas palavras!"/>
-            <button>aa</button>
-          </div>
+            <p className="md:basis-2/5 max-w h-20 p-5 md:text-9xl text-7xl">SCRAMBLE</p>  
+          </div> {/* usar imagem para a logo? */}
 
+          <div className="mb-5r flex flex-col w-1/2 items-center border-2 border-amber-300">
+            <input type="text" id="words" className="md:w-full h-20 p-5 bg-amber-200 text-pink-500 text-m rounded-full hover:border-4 hover:border-amber-600" 
+            placeholder="Digite suas palavras!"/>
+            <button className='self-end bg-[rgb(233,122,111)] px-15 py-2 m-10 rounded-3xl shadow-md hover:bg-[rgb(233,122,255)] '>SEND</button>
+          </div>
+        </div>
+        <div className='flex flex-col justify-center items-center w-screen'>
+          <div className='w-1/2 p-2.5 bg-transparent rounded-3xl border-4 border-amber-500'></div>
         </div>
     </form>
     </>
